b2d916991e6a3aa0be87b6d7c41ad12b87c4876d
Duy Dev <duy99.uit@gmail.com>
2022-03-16 09:21:23 +0700

    fix homescreen

--- src/components/HomeSection/index.tsx
+++ src/components/HomeSection/index.tsx
@@ -2,21 +2,13 @@ import dior from '@/assets/images/dior.jpg';
 import dior111 from '@/assets/images/dior111.jpg';
 import diorMen from '@/assets/images/dior-men.jpg';
 
-/* This example requires Tailwind CSS v2.0+ */
 function HomeSection() {
   return (
-    <div className="relative bg-white  max-h-screen h-auto md:h-screen overflow-hidden">
+    <div className="relative bg-white  max-h-screen h-auto lg:h-screen overflow-hidden">
       <div className="pt-4 pb-80 sm:pt-0 sm:pb-40 lg:pt-0 lg:pb-48">
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static md:flex md:flex-row">
-          <div className="md:w-3/5  ">
+          <div className="md:w-3/5 h-auto ">
             <div className="py-0 flex justify-center items-center flex-column flex-wrap">
-              <div className="w-full overflow-hidden lg:opacity-100">
-                <img
-                  src={dior}
-                  alt=""
-                  className="w-full h-full object-center object-cover"
-                />
-              </div>
               <div className="w-full py-4  flex flex-wrap flex-row items-center justify-between pr-16">
                 <div>
                   <p className="mt-4 text-4xl text-gray-500 font-bold ">
@@ -34,18 +26,39 @@ function HomeSection() {
                   Get Started
                 </button>
               </div>
+              <div className="w-full pr-2 overflow-hidden lg:opacity-100">
+                <img
+                  src={dior}
+                  alt=""
+                  className="w-full h-full rounded-lg object-center object-cover"
+                />
+              </div>
             </div>
           </div>
-          <div className="md:w-2/5 h-auto">
-            <div className=" bg-gray-100 ">
-              <div className="w-full h-screen md:h-auto rounded-b-lg overflow-hidden lg:opacity-100 ">
+
+          <div className="md:w-2/5 h-screen">
+            <div className="h-2/3">
+              <div className="w-full  md:h-full mt-8 overflow-hidden lg:opacity-100 ">
                 <img
                   src={dior111}
                   alt=""
-                  className="w-full h-full object-center object-cover"
+                  className="w-full h-full  rounded-lg object-center object-cover"
                 />
               </div>
-              {/* <p className="mt-4 text-3xl text-gray-500">Dior x Air Jordan</p> */}
+              <div className="w-full flex flex-wrap flex-row items-center justify-between mt-2">
+                <div className="ml-2 flex-1 pr-2">
+                  <p className="mt-2 text-md text-gray-700 font-normal  ">
+                  Dior branded tongue and Wings logo, and a silver “Air Dior” hang tag completes the design. 
+                  </p>
+                </div>
+
+                <button
+                  type="submit"
+                  className=" bg-transparent border border-gray-600 rounded-md py-3 px-12 flex items-center justify-center text-base font-medium text-gray-600 hover:bg-gray-200  focus:outline-none hover:text-gray-700"
+                >
+                  Pre order
+                </button>
+              </div>
             </div>
           </div>
         </div>
